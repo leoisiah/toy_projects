c5c5462a4361a03f546d83a39c079565364588cd
leoisiah <leoisiah@yahoo.com>
2024-02-29 14:00:18 +0800

    Add files via upload

---
+++ manila_paper.py
@@ -0,0 +1,18 @@
+from unidecode import unidecode;
+import pandas;
+import requests;
+
+dfs = pandas.read_html("https://www.worlddata.info/capital-cities.php");
+for df in dfs:
+    cities = df['Capital City'];
+    for city in cities:        
+        city_name = city.replace(" ", "_");
+        city_name = unidecode(city_name);
+        r = requests.get("https://en.wiktionary.org/w/index.php?title="+city_name+"_paper");
+        if r.status_code==200:
+            print(city);
+        else:
+            r = requests.get("https://en.wiktionary.org/w/index.php?title="+city_name.lower()+"_paper");
+            if r.status_code==200:
+                print(city);
+            
