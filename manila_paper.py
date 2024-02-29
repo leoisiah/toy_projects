19b028392b374338f5edb36ffeace95b234380e5
leoisiah <leoisiah@yahoo.com>
2024-02-29 15:21:21 +0800

    Update manila_paper.py
    For performance

--- manila_paper.py
+++ manila_paper.py
@@ -2,17 +2,18 @@ from unidecode import unidecode;
 import pandas;
 import requests;
 
+req = requests.Session()
 dfs = pandas.read_html("https://www.worlddata.info/capital-cities.php");
 for df in dfs:
     cities = df['Capital City'];
     for city in cities:        
         city_name = city.replace(" ", "_");
         city_name = unidecode(city_name);
-        r = requests.get("https://en.wiktionary.org/w/index.php?title="+city_name+"_paper");
+        r = req.get("https://en.wiktionary.org/w/index.php?title="+city_name+"_paper");
         if r.status_code==200:
             print(city);
         else:
-            r = requests.get("https://en.wiktionary.org/w/index.php?title="+city_name.lower()+"_paper");
+            r = req.get("https://en.wiktionary.org/w/index.php?title="+city_name.lower()+"_paper");
             if r.status_code==200:
                 print(city);
             
